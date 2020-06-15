0813e7c3fbda52fe1e866d6a70154cc065857948
Tobias Breinhild <tbbr.skp@edu.mercantec.dk>
2020-06-15 15:10:25 +0200

    Started Project

---
+++ MazeShooter/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var inputX = Input.GetAxis("Horizontal");
+        var inputY = Input.GetAxis("Vertical");
+
+        Vector2 position = transform.position;
+        position.x += speed * Time.deltaTime * inputX;
+        position.y += speed * Time.deltaTime * inputY;
+
+        transform.position = position;
+    }
+}
