6b631b0e6eecc5f72518d8e3d3df84ed604dc012
Tobias Breinhild <tbbr.skp@edu.mercantec.dk>
2020-06-16 12:12:04 +0200

    Player Functionality and Map Complete

---
+++ MazeShooter/Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var playerPosition = player.position;
+        playerPosition.z = transform.position.z;
+        transform.position = playerPosition;
+    }
+}
---
+++ MazeShooter/Assets/CameraSmooth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSmooth : MonoBehaviour
+{
+    public Transform player;
+
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var playerPosition = player.position;
+
+        playerPosition.z = transform.position.z;
+
+        var target = Vector3.SmoothDamp(transform.position, playerPosition, ref velocity, 0.5f);
+
+        transform.position = target;
+    }
+}
