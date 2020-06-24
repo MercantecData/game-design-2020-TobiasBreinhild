1251196a532cd8f0763f5d90cdcccef19ba16668
Tobias Breinhild <tbbr.skp@edu.mercantec.dk>
2020-06-24 09:41:37 +0200

    Added Sound Effect to Enemies. Finally!

--- MazeShooter/Assets/Scripts/EnemyHealth.cs
+++ MazeShooter/Assets/Scripts/EnemyHealth.cs
@@ -37,6 +37,7 @@ public class EnemyHealth : MonoBehaviour
     {
         if (collision.tag == "Bullet")
         {
+            FindObjectOfType<AudioManager>().Play("EnemyDeathSound");
             GotHitTrue();
             enemyHP -= 1;
             Invoke("GotHitFalse", 1f);
